c82afaaa7a9f1de295a9eba8253073c03ba36b35
noah-tah <noahtah21@gmail.com>
2024-07-05 13:27:30 -0500

    solved the revereString and i honestly feel like my solution was pretty good

---
+++ 03_reverseString/notes.md
@@ -0,0 +1,8 @@
+so to reverse a string we would break it down into all of its letters
+
+then we would take the last letter off of the string and store it somehwere into a variable
+
+continue doing this for each character in the string
+
+
+ok what if i solved it like this: find the length of the string, then iterate over the length, storing the next character into memory and then appending it to the reversed string
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
-// Do not edit below this line
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let reversedString = '';
+    for (let i = stringLength - 1; i >= 0; i--) {
+        let currentChar = string[i];
+        reversedString += currentChar;
+    }
+    return reversedString;
+}; reverseString("hello") // Do not edit below this line
 module.exports = reverseString;
