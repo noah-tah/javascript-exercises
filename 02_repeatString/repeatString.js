4723a07bb49a891c1df3d30d9941d89afe97f4ba
noah-tah <noahtah21@gmail.com>
2024-07-05 11:39:59 -0500

    finished part 2 of the exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        console.log('ERROR');
+        return 'ERROR';
+    }
+    let stringToReturn = "";
+    for (i = 0 ; i < num; i++) {
+        stringToReturn += string;
+    }
+    return stringToReturn;
 };
 
+repeatString('hey', 3);
+
+repeatString('hello', 10);
+
+repeatString('hi', 1);
+
+repeatString('bye', 0);
+
+repeatString('goodbye', -1);
+
+repeatString('odin', Math.random());
+
+repeatString('', 10);
 // Do not edit below this line
 module.exports = repeatString;
