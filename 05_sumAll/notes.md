a342fe2f99665c3c585ced7e660cdb9b2c00cbe9
noah-tah <noahtah21@gmail.com>
2024-07-06 05:29:26 -0500

    finished sumAll

---
+++ 05_sumAll/notes.md
@@ -0,0 +1,14 @@
+# How I would do this on pen and paper.
+
+Sum of Integers Formula
+    $ S = \fract{n(a+1)}{2}$     
+    S =( (n(a+1)) / (2))
+
+1 4
+1 + 2 + 3 + 4 = 10
+
+1(1 + 1) / 2 = 1
+
+2(1 + 1)/ 2 = 2
+
+3(1 + 1) / 2 = 3
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,37 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // ✕ returns ERROR with negative numbers (1 ms)
+    // ✕ returns ERROR with non-number parameters
+    // ✕ returns ERROR with non-number parameters
+    if (!Number.isInteger(a)) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(b)){
+        return "ERROR";
+    }
+
+    if (a < 0) {
+        return "ERROR";
+    }
+
+    if (b < 0) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (let i = a; i <= b; i++) {
+       sum += i; 
+    }
+    return sum;
+
 
 };
+sumAll(4, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
