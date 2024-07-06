70cc67f0e43a578a09d406b926da5454d3b24f40
noah-tah <noahtah21@gmail.com>
2024-07-06 10:55:04 -0500

    finished with simple control flow logic, didn't have to use && operator, but confident i could solve it that way too

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if (years % 400 === 0) {
+        return true;
+    } else if (years % 100 === 0) {
+        return false;
+    } else if (years % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
+
---
+++ 06_leapYears/notes.md
@@ -0,0 +1,4 @@
+# Leap Years
+- Divisible by 4.
+- Not divisible by 100.
+    - Unless divisible by 400.
