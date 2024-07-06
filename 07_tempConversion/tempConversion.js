a991ba33221e1de7d1c47fb9326e35f7f2dd576d
noah-tah <noahtah21@gmail.com>
2024-07-06 15:03:18 -0500

    completed final exercise for arrays and loops

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  // F to C C = 5/9(F-32);
+  let a = 5 / 9;
+  let b = temperatureInFahrenheit - 32;
+  let c = a * b;
+  if (c === 0) {
+    return c;
+  } else {
+    return parseFloat(c.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  let a = 9 / 5;
+  let b = a * temperatureInCelsius;
+  let c = b + 32;
+  if (c === 0) {
+    return c;
+  } else {
+    return parseFloat(c.toFixed(1));
+  }
 };
 
 // Do not edit below this line
