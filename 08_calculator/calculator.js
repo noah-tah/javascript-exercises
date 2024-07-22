6a8d3308a23a23acd74c7d99f6c4444c03599e80
noah-tah <noahtah21@gmail.com>
2024-07-22 07:45:39 -0500

    finished calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = arr.reduce((sum, current) => sum + current, 0);
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let result = arr.reduce((product, current) => product * current);
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
