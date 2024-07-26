199e1c84f742271f63086ff66f26453e6f7f4988
noah-tah <noahtah21@gmail.com>
2024-07-26 07:21:59 -0500

    palindrome completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+// needs to working with symbols
+// needs to work with uppercase
+// needs to work with multiple words
+const palindromes = function(string) {
+  const alphanumerical = 'abcdefghijklmnpqrstuvwxyz0123456789';
+  let cleanedString = string
+    .toLowerCase()
+    .split('')
+    .join('')
+
+  let reversedString = cleanedString.split('').reverse().join('');
+
+  if (reversedString === cleanedString) {
+    return true;
+  } else {
+    return false;
+  };
 
 };
 
